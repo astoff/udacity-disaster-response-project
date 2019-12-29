c852ca9adff0924980e0d63c34dc89de403404e6
Augusto Stoffel <astoff@users.noreply.github.com>
2019-12-29 17:25:16 +0100

    Add docstrings

--- data/process_data.py
+++ data/process_data.py
@@ -5,24 +5,57 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Read the messsages and categories CSV files, and returns one big
+    dataframe containing the text and the categories of each message.
+
+    Parameters
+    ----------
+    message_filepath
+        path to the “disaster_messages.csv” file
+    categories_filepath
+        path to the “disaster_categories.csv” file
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+
     # Read original files
     messages = pd.read_csv(messages_filepath, index_col='id')
     categories = pd.read_csv(categories_filepath, index_col='id')
+
     # Make categories column a list of strings of the form ⟨category⟩-⟨value⟩
     categories = pd.DataFrame(categories.iloc[:, 0].str.split(';').tolist(),
                               index=categories.index)
+
     # Change each column name to ⟨category⟩
     categories.columns = [re.sub('-.*', '', x) for x in categories.iloc[0]]
+
     # Strip ⟨category⟩ and convert to number.
     for column in categories:
         categories[column] = categories[column].map(
             lambda x: int(re.sub('.*-', '', x)))
+
     # Return a single dataframe with the messages and labels
     df = pd.merge(messages, categories, left_index=True, right_index=True)
     return df
 
 
 def clean_data(df):
+    """
+    Perform some data cleaning on the disaster response dataset,
+    mostly dropping duplicates.
+
+    Arguments
+    ---------
+    df
+        the dataframe returned by load_data
+
+    Returns
+    -------
+    pd.DataFrame
+    """
     df = df.drop_duplicates()
     # In a few places, the value 2 appears for the "related" column.
     # We change this to 1, since some classification algorithms
@@ -32,11 +65,26 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    Save a dataframe as a sqlite database.
+
+    Arguments
+    ---------
+    df
+        a pandas dataframe
+    database_filename
+        the file name to use
+    """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('DisasterResponse', engine, index=False)
 
 
 def main():
+    """
+    The main loop of this script.  Runs load_data, clean_data and
+    save_data.  Running it without arguments will teach you which
+    command-line arguments are needed.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -15,7 +15,22 @@ nltk.download('punkt')
 
 
 def load_data(database_filepath):
-    engine = create_engine('sqlite:///' + database_filepath)
+    """
+    Load the disaster response dataset from an sqlite database.
+
+    Returns
+    -------
+    X : pd.DataFrame
+        A dataframe containing a single columns, “message”, with the
+        complete message text as a string.
+    Y : pd.DataFrame
+        A dataframe with several columns, with columns named after
+        message categories, indicating whethere the corresponding
+        message belongs to that category (value 0 or 1).
+    columns
+        A list of category names (just the columns of Y)
+    """
+    engine =  create_engine('sqlite:///' + database_filepath)
     df = pd.read_sql('DisasterResponse', engine)
     X = df['message']
     Y = df.drop(columns=['message', 'original', 'genre'])
@@ -26,15 +41,30 @@ stem = SnowballStemmer("english").stem
 
 
 def tokenize(text):
+    """
+    Turn a string into a list of words, with stemming.
+    """
     words = word_tokenize(text.lower())
     words = [stem(w) for w in words]
     return words
 
 
+# Since we roll our own tokenizer, we need to produce our own list of
+# stop words, by applying the stemming to a usual stopword
+# list.
 stop_words = [stem(w) for w in
               TfidfVectorizer(stop_words="english").get_stop_words()]
 
 def build_model():
+    """
+    Returns a sklearn classifier ready to be trained on the data.
+    Note that this classifier pipeline includes the vectorizer, to it
+    consumes message text directly.
+
+    Returns
+    -------
+    an sklearn pipeline object
+    """
     pipeline = Pipeline([
         ("vectorizer", TfidfVectorizer(tokenizer=tokenize,
                                        stop_words=stop_words)),
@@ -45,6 +75,13 @@ def build_model():
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Print performance reports for the given model, using X_test and Y_test.
+
+    Argument “category_names” is not used, but we keep it to remain
+    compatible with Udacity's project stub (It was supposed to be
+    Y_test.columns).
+    """
     Y_pred = pd.DataFrame(model.predict(X_test),
                           index=Y_test.index,
                           columns=Y_test.columns)
@@ -58,12 +95,20 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    """
+    Save a pickled copy of model to the specified file.
+    """
     s = pickle.dumps(model)
     with open(model_filepath, "wb") as f:
         f.write(s)
 
 
 def main():
+    """
+    The main loop of this script.  Load the data, trains a model, and
+    saves it.  Running the script without command-line arguments will
+    teach you which arguments are needed.
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
