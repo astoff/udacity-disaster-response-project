10d572fed84b1e3cc0645d0d6c985a8c914a3b01
Augusto Stoffel <astoff@users.noreply.github.com>
2019-12-29 17:00:44 +0100

    Adjust paths to database and pickled model

--- app/run.py
+++ app/run.py
@@ -22,11 +22,11 @@ def tokenize(text):
     return words
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../out/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../out/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -38,7 +38,7 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
