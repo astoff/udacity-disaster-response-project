add67eb1e14801f836b3f90b01d145d60abec06b
Augusto Stoffel <astoff@users.noreply.github.com>
2019-12-29 16:59:37 +0100

    Fill in my model-training code

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,66 @@
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sqlalchemy import create_engine
+import nltk
+import pandas as pd
+import pickle
 import sys
+
+nltk.download('punkt')
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql('DisasterResponse', engine)
+    X = df['message']
+    Y = df.drop(columns=['message', 'original', 'genre'])
+    return X, Y, Y.columns
+
+
+stem = SnowballStemmer("english").stem
 
 
 def tokenize(text):
-    pass
+    words = word_tokenize(text.lower())
+    words = [stem(w) for w in words]
+    return words
 
+
+stop_words = [stem(w) for w in
+              TfidfVectorizer(stop_words="english").get_stop_words()]
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ("vectorizer", TfidfVectorizer(tokenizer=tokenize,
+                                       stop_words=stop_words)),
+        ("classifier", RandomForestClassifier(n_estimators=100,
+                                              max_features=0.1,
+                                              n_jobs=-1))])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = pd.DataFrame(model.predict(X_test),
+                          index=Y_test.index,
+                          columns=Y_test.columns)
+
+    for col in Y_pred.columns:
+        print("Report for category “%s”:" % col)
+        print(classification_report(Y_test[col], Y_pred[col]))
+
+    print("Summary report:")
+    print(classification_report(Y_test, Y_pred))
 
 
 def save_model(model, model_filepath):
-    pass
+    s = pickle.dumps(model)
+    with open(model_filepath, "wb") as f:
+        f.write(s)
 
 
 def main():
@@ -26,14 +68,15 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -43,9 +86,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
